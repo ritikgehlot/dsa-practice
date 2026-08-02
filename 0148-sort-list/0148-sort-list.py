fddb8b7ace65b7aa2daf7ead92c09f7459245f16
ritik <gehlotritik2007@gmail.com>
2026-08-02 22:15:19 +0530

    Time: 166 ms (69.92%), Space: 40.5 MB (98.17%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,30 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        dummy = ListNode()
+        cur = dummy
+
+        while left and right:
+            if left.val < right.val:
+                cur.next = left
+                left = left.next
+            else:
+                cur.next = right
+                right = right.next
+            cur = cur.next
+
+        cur.next = left or right
+        return dummy.next
