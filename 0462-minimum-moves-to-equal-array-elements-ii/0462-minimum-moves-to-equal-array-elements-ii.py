c73c83e0d804ad46c24d776a58e85d3da94531fb
ritik <gehlotritik2007@gmail.com>
2026-08-16 14:10:30 +0530

    Time: 3 ms (80.83%), Space: 20.5 MB (42.64%) - LeetHub

---
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+
+        median = nums[len(nums) // 2]
+
+        moves = 0
+
+        for num in nums:
+            moves += abs(num - median)
+
+        return moves
