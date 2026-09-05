7b485d0cc52f4dfbc8965dc37923825c7c99b793
ritik <gehlotritik2007@gmail.com>
2026-09-05 12:08:46 +0530

    Time: 25 ms (40.86%), Space: 20.6 MB (29.62%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,10 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        mx = -1
+
+        for i in range(len(arr) - 1, -1, -1):
+            current = arr[i]
+            arr[i] = mx
+            mx = max(mx, current)
+
+        return arr
