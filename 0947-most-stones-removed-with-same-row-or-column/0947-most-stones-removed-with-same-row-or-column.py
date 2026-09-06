73aff284de098dd4ff2402e428cc88fe88efc081
ritik <gehlotritik2007@gmail.com>
2026-09-06 21:50:08 +0530

    Time: 652 ms (22.29%), Space: 19.7 MB (78.53%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,38 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
+
+        parent = list(range(n))
+        size = [1] * n
+
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+
+        def union(a, b):
+            a = find(a)
+            b = find(b)
+
+            if a == b:
+                return
+
+            if size[a] < size[b]:
+                a, b = b, a
+
+            parent[b] = a
+            size[a] += size[b]
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
+                    union(i, j)
+
+        components = 0
+
+        for i in range(n):
+            if find(i) == i:
+                components += 1
+
+        return n - components
