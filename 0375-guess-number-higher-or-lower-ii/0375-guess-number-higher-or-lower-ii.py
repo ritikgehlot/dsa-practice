460ee331bc75b06965adc69f8e364675740285cf
ritik <gehlotritik2007@gmail.com>
2026-09-14 21:24:46 +0530

    Time: 1009 ms (61.52%), Space: 20 MB (74.3%) - LeetHub

---
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        dp = [[0] * (n + 2) for _ in range(n + 2)]
+
+        for length in range(2, n + 1):
+            for left in range(1, n - length + 2):
+                right = left + length - 1
+                dp[left][right] = float('inf')
+
+                for x in range(left, right):
+                    cost = x + max(
+                        dp[left][x - 1],
+                        dp[x + 1][right]
+                    )
+                    dp[left][right] = min(
+                        dp[left][right],
+                        cost
+                    )
+
+        return dp[1][n]
