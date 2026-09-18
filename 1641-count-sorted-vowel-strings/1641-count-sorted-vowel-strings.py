9a96ac44a953c29ad261a37d2a481475e79f1b08
ritik <gehlotritik2007@gmail.com>
2026-09-18 21:37:52 +0530

    Time: 0 ms (100%), Space: 19.4 MB (19.18%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        dp = [1] * 5
+
+        for _ in range(n - 1):
+            for i in range(3, -1, -1):
+                dp[i] += dp[i + 1]
+
+        return sum(dp)
