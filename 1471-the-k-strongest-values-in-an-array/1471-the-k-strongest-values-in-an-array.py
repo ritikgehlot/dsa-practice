aad75869c825b20d28979f9dcc82f0d9ab20025d
ritik <gehlotritik2007@gmail.com>
2026-09-22 21:49:01 +0530

    Time: 281 ms (19%), Space: 37.7 MB (24.5%) - LeetHub

---
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getStrongest(self, arr: List[int], k: int) -> List[int]:
+        median = sorted(arr)[(len(arr) - 1) // 2]
+
+        arr.sort(key=lambda x: (abs(x - median), x), reverse=True)
+
+        return arr[:k]
